54bf0345a18af021844af9a2dbb5eb719c987e3b
Wayne Barber <68376947+wbarber93@users.noreply.github.com>
2021-01-07 18:52:55 +0000

    Add files via upload

---
+++ Simple_Stock_Predictor.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as mpl
+from sklearn.preprocessing import scale
+from TFANN import ANNR
+from google.colab import files
+
+files.upload()
+
+stock_data = np.loadtxt('ZBH_5y.csv', delimiter=",", skiprows=1, usecols=(1, 4))
+stock_data=scale(stock_data)
+
+prices = stock_data[:, 1].reshape(-1, 1)
+dates = stock_data[:, 0].reshape(-1, 1)
+
+mpl.plot(dates[:, 0], prices[:, 0])
+mpl.show()
+
+#neurons in each layer
+input = 1
+output = 1
+hidden = 50
+
+layers = [('F', hidden), ('AF', 'tanh'), ('F', hidden), ('AF', 'tanh'), ('F', hidden), ('AF', 'tanh'), ('F', output)]
+
+#construct the model and dictataQe params
+mlpr = ANNR([input], layers, batchSize = 256, maxIter = 20000, tol = 0.2, reg = 1e-4, verbose = True)
+
+
+#number of days for the hold-out period used to access progress
+holdDays = 5
+totalDays = len(dates)
+
+#fit the model to the data "Learning"
+mlpr.fit(dates[0:(totalDays-holdDays)], prices[0:(totalDays-holdDays)])
+
+
+#Predict the stock price using the model
+pricePredict = mlpr.predict(dates)
+
+#Display the predicted reuslts agains the actual data
+mpl.plot(dates, prices)
+mpl.plot(dates, pricePredict, c='#5aa9ab')
+mpl.show()
+
