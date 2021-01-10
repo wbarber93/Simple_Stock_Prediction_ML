1013cf6f887e5ee60909826190793c0dba0e6f76
Wayne Barber <68376947+wbarber93@users.noreply.github.com>
2021-01-10 10:57:18 +0000

    Update Simple_Stock_Predictor.py

--- Simple_Stock_Predictor.py
+++ Simple_Stock_Predictor.py
@@ -6,7 +6,7 @@ from google.colab import files
 
 files.upload()
 
-stock_data = np.loadtxt('ZBH_5y.csv', delimiter=",", skiprows=1, usecols=(1, 4))
+stock_data = np.loadtxt('stockinfo.csv', delimiter=",", skiprows=1, usecols=(1, 4))
 stock_data=scale(stock_data)
 
 prices = stock_data[:, 1].reshape(-1, 1)
@@ -15,26 +15,26 @@ dates = stock_data[:, 0].reshape(-1, 1)
 mpl.plot(dates[:, 0], prices[:, 0])
 mpl.show()
 
-#neurons in each layer
+# Neurons in each layer
 input = 1
 output = 1
 hidden = 50
 
 layers = [('F', hidden), ('AF', 'tanh'), ('F', hidden), ('AF', 'tanh'), ('F', hidden), ('AF', 'tanh'), ('F', output)]
 
-#construct the model and dictataQe params
+# Construct the model and dictataQe params
 mlpr = ANNR([input], layers, batchSize = 256, maxIter = 20000, tol = 0.2, reg = 1e-4, verbose = True)
 
 
-#number of days for the hold-out period used to access progress
+# Specify hold out period
 holdDays = 5
 totalDays = len(dates)
 
-#fit the model to the data "Learning"
+# Fit the model to the data - "Learning"
 mlpr.fit(dates[0:(totalDays-holdDays)], prices[0:(totalDays-holdDays)])
 
 
-#Predict the stock price using the model
+# Predict the stock price using the model
 pricePredict = mlpr.predict(dates)
 
 #Display the predicted reuslts agains the actual data
